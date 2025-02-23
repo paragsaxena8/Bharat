2c82c2002b362a88f7a441dfee989fa8e3521cb9
Parag Saxena <rckngprg@gmail.com>
2025-02-23 16:30:15 +0530

    isotope-layout fix

--- src/app/layout/projects/project.component.ts
+++ src/app/layout/projects/project.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import Isotope from 'isotope-layout';
 
 @Component({
   selector: 'app-project',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
-
+  @ViewChild('gridContainer', { static: false }) gridContainer!: ElementRef;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  isotope!: Isotope;
+  activeFilter = '*';
+
+  filters = [
+    { label: 'All Projects', value: '*' },
+    { label: 'Solar Energy', value: '.seo' },
+    { label: 'Wind Energy', value: '.marketing' },
+    { label: 'Hydropower Plants', value: '.website' }
+  ];
+
+  projects = [
+    { name: 'Solar Project 1', category: 'seo', url: '', image: 'assets/img/gallery/protfolio-img01.png' },
+    { name: 'Wind Project 1', category: 'marketing', url: '', image: 'assets/img/gallery/protfolio-img02.png' },
+    { name: 'Hydropower Project 1', category: 'website', url: '', image: 'assets/img/gallery/protfolio-img03.png' },
+    { name: 'Solar Project 2', category: 'seo', url: '', image: 'assets/img/gallery/protfolio-img04.png' },
+    { name: 'Wind Project 2', category: 'marketing', url: '', image: 'assets/img/gallery/protfolio-img05.png' },
+  ];
+
+  ngAfterViewInit() {
+    setTimeout(() => {  // Ensure Isotope initializes after DOM rendering
+      this.isotope = new Isotope(this.gridContainer.nativeElement, {
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'  // Ensures items are arranged in rows
+      });
+    }, 100);
+  }
+
+  applyFilter(filterValue: string) {
+    this.activeFilter = filterValue;
+    this.isotope.arrange({ filter: filterValue });
   }
 
 }
